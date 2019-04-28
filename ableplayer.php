004bc5cfb86386d44cfd2b866ede741b2cba3126
Terrill <terrill@terrillthompson.com>
2019-04-27 17:12:01 -0700

    Add support for shortcodes

--- ableplayer.php
+++ ableplayer.php
@@ -5,8 +5,7 @@ Plugin URI: https://github.com/ableplayer-wordpress
 Description: Accessible HTML5 media player
 Contributors: terrillthompson
 Tags: html5, media, audio, video, accessibility
-Version: 0.1
-Stable tag: 0.1
+Version: 0.1.1
 Requires at least: 2.6
 Tested up to: 4.5
 License: MIT
@@ -40,29 +39,291 @@ function ableplayer_enqueue_scripts(){
   wp_enqueue_script( 'jquery' );
 
   // Register/enqueue other dependencies
-  wp_enqueue_script( 'modernizer', plugins_url('thirdparty',__FILE__).'/modernizr.custom.js');
   wp_enqueue_script( 'js-cookie', plugins_url('thirdparty',__FILE__).'/js.cookie.js',array('jquery'));
 
-  // Register/enqueue Able Player script
+  // Register/enqueue Able Player JavaScript (two options; uncomment the one you prefer)
+
+  // JS Option 1: minified, for production
+  // wp_enqueue_script( 'ableplayer', plugins_url('build',__FILE__).'/ableplayer.min.js',array('jquery'));
+
+  // JS Option 2: human-readable, for debugging
   wp_enqueue_script( 'ableplayer', plugins_url('build',__FILE__).'/ableplayer.js',array('jquery'));
 
   // Register/enqueue Able Player CSS (two options; uncomment the one you prefer)
 
-  // Use minimized CSS that's included in Able Player build
-  // wp_enqueue_style( 'ableplayer', plugins_url('build',__FILE__).'/ableplayer.min.css');
+  // CSS Option 1: minified, for production
+  wp_enqueue_style( 'ableplayer', plugins_url('build',__FILE__).'/ableplayer.min.css');
 
-  // Use human-readable CSS if you intend to change the styles and customize the player
-  wp_enqueue_style( 'ableplayer', plugins_url('styles',__FILE__).'/ableplayer.css');
+  // CSS Option 2: human-readable; use this if you intend to change the styles and customize the player
+  // wp_enqueue_style( 'ableplayer', plugins_url('styles',__FILE__).'/ableplayer.css');
 
 }
 add_action( 'wp_enqueue_scripts', 'ableplayer_enqueue_scripts');
 
-function ableplayer_head() {
 
-  // add a variable that points to the current plugin directory
-  echo "<script>\n";
-  echo 'var pluginUrl = "'.plugins_url('',__FILE__).'";'."\n";
-  echo "</script>\n";
+/*
+ *
+ * Add support for [able-player] shortcode
+ *
+ *
+*/
+
+function able_player_shortcode( $atts,$content=null ) {
+
+	// Each of the following attributes can be passed with the [able-player] shortcode
+	// 'id' and 'type' (video or audio) is required
+
+  // normalize attribute keys, lowercase
+  $atts = array_change_key_case((array)$atts, CASE_LOWER);
+
+  // build complete array of all attributes; defaults will be overridden with user values
+  $all_atts = shortcode_atts([
+    'id' => get_unique_id(),
+		'type' => '',
+		'autoplay' => 'false',
+		'loop' => 'false',
+		'playsinline' => 'true',
+		'hidecontrols' => 'false',
+		'poster' => '',
+		'width' => '',
+		'height' => '',
+		'heading' => '',
+		'speed' => 'animals',
+		'start' => '',
+		'volume' => '',
+		'seekinterval' => '',
+		'nowplaying' => 'false'
+	], $atts );
+
+  // output
+  if (!$all_atts['type']) {
+    // required fields are missing
+    return false;
+  }
+  else {
+    $type = $all_atts['type'];
+    if (!($type == 'audio' || $type == 'video')) {
+      // type is not a supported value
+      return false;
+    }
+    else {
+      // build a media player!
+      $o = '<'.$type;
+      $o .= ' id="'.$all_atts['id'].'"';
+      $o .= ' data-able-player';
+      $o .= ' preload="auto"';
+      if (is_true($all_atts['autoplay'])) {
+        $o .= ' autoplay';
+      }
+      if (is_true($all_atts['loop'])) {
+        $o .= ' loop';
+      }
+      if (is_true($all_atts['playsinline'])) {
+        $o .= ' playsinline';
+      }
+      if (is_true($all_atts['hidecontrols'])) {
+        $o .= ' data-hide-controls';
+      }
+      if (!empty($all_atts['poster'])) {
+        $o .= ' poster="'.$all_atts['poster'].'"';
+      }
+      if (!empty($all_atts['width'])) {
+        $o .= ' width="'.$all_atts['width'].'"';
+      }
+      if (!empty($all_atts['height'])) {
+        $o .= ' height="'.$all_atts['height'].'"';
+      }
+      if (!empty($all_atts['poster'])) {
+        $o .= ' poster="'.$all_atts['poster'].'"';
+      }
+      if (!empty($all_atts['heading'])) {
+        $o .= ' data-heading-level="'.$all_atts['heading'].'"';
+      }
+      if (!empty($all_atts['speed'])) {
+        $o .= ' data-speed-icons="'.$all_atts['speed'].'"';
+      }
+      if (!empty($all_atts['start'])) {
+        $o .= ' data-start-time="'.$all_atts['start'].'"';
+      }
+      if (!empty($all_atts['volume'])) {
+        $o .=  'data-volume="'.$all_atts['volume'].'"';
+      }
+      if (!empty($all_atts['seekinterval'])) {
+        $o .= ' data-seek-interval="'.$all_atts['seekinterval'].'"';
+      }
+      if (!empty($all_atts['shownowplaying'])) {
+        $o .= ' data-show-now-playing="'.$all_atts['shownowplaying'].'"';
+      }
+      $o .= '>';
+
+      // enclosing tags
+      if (!is_null($content)) {
+        // run shortcode parser recursively
+        $o .= do_shortcode($content);
+      }
+
+      // end media tag
+      $o .= '</'.$type.'>';
+
+      // return output
+      // To display HTML <audio> or <video> code (for debugging), uncomment first return statement
+      // For production, uncomment second return statement
+      // return esc_html($o);
+      return $o;
+    }
+  }
 }
-add_action('wp_head','ableplayer_head',3);
+add_shortcode('able-player', 'able_player_shortcode');
+
+/*
+ *
+ * Add support for [able-source] shortcode
+ *
+ *
+*/
+
+function able_source_shortcode( $atts,$content=null ) {
+
+	// Each of the following attributes can be passed with the [able-source] shortcode
+	// Either 'src', youtube-id, or vimeo-id is required
+
+  $all_atts = shortcode_atts([
+		'src' => '',
+		'type' => '',
+		'desc-src' => '',
+		'sign-src' => '',
+		'youtube-id' => '',
+		'youtube-desc-id' => '',
+		'youtube-nocookie' => '',
+		'vimeo-id' => '',
+		'vimeo-desc-id' => ''
+	], $atts );
+
+  // normalize attribute keys, lowercase
+  $atts = array_change_key_case((array)$atts, CASE_LOWER);
+
+  // output
+  if (!($all_atts['src'] || $all_atts['youtube-id'] || $all_atts['youtube-desc-id'])) {
+    // there is no media source
+    return false;
+  }
+  else {
+    // build a source tag
+    $o = '<source';
+    if (!empty($all_atts['src'])) {
+      $o .= ' src="'.$all_atts['src'].'"';
+    }
+    if (!empty($all_atts['type'])) {
+      $o .= ' type="'.$all_atts['type'].'"';
+    }
+    if (!empty($all_atts['desc-src'])) {
+      $o .= ' data-desc-src="'.$all_atts['desc-src'].'"';
+    }
+    if (!empty($all_atts['sign-src'])) {
+      $o .= ' data-sign-src="'.$all_atts['sign-src'].'"';
+    }
+    if (!empty($all_atts['youtube-id'])) {
+      $o .= ' data-youtube-id="'.$all_atts['youtube-id'].'"';
+    }
+    if (!empty($all_atts['youtube-desc-id'])) {
+      $o .= ' data-youtube-desc-id="'.$all_atts['youtube-desc-id'].'"';
+    }
+    if (!empty($all_atts['youtube-nocookie'])) {
+      $o .= ' data-youtube-nocookie="'.$all_atts['youtube-nocookie'].'"';
+    }
+    if (!empty($all_atts['vimeo-id'])) {
+      $o .= ' data-vimeo-id="'.$all_atts['vimeo-id'].'"';
+    }
+    if (!empty($all_atts['vimeo-desc-id'])) {
+      $o .= ' data-vimeo-desc-id="'.$all_atts['vimeo-desc-id'].'"';
+    }
+    $o .= '>';
+  }
+  return $o;
+}
+add_shortcode('able-source', 'able_source_shortcode');
+
+/*
+ *
+ * Add support for [able-track] shortcode
+ *
+ *
+*/
+
+function able_track_shortcode( $atts,$content=null ) {
+
+	// Each of the following attributes can be passed with the [able-track] shortcode
+	// 'kind' and 'src' are required
+
+  $all_atts = shortcode_atts([
+		'kind' => '',
+		'src' => '',
+		'srclang' => '',
+		'label' => ''
+	], $atts );
+
+  // normalize attribute keys, lowercase
+  $atts = array_change_key_case((array)$atts, CASE_LOWER);
+
+  // output
+  if (!($all_atts['kind'] && $all_atts['src'])) {
+    // required fields are missing
+    return false;
+  }
+  else {
+    // build a track tag
+    $o = '<track';
+    $o .= ' kind="'.$all_atts['kind'].'"';
+    $o .= ' src="'.$all_atts['src'].'"';
+    if (!empty($all_atts['srclang'])) {
+      $o .= ' srclang="'.$all_atts['srclang'].'"';
+    }
+    if (!empty($all_atts['label'])) {
+      $o .= ' label="'.$all_atts['label'].'"';
+    }
+    $o .= '>';
+    return $o;
+  }
+}
+add_shortcode('able-track', 'able_track_shortcode');
+
+/*
+ *
+ * Supporting functions
+ *
+*/
+
+function get_unique_id() {
+
+  // use add_option(), get_option(), update_option(), & delete_option()
+  // to track the number of Able Player instances on the current page
+  // return a unique id for each new player instance,
+  // using the form 'able_player_1', 'able_player_2', etc.
+  $this_player = 1;
+  $num_players = get_option('able-player-count');
+  if (empty($num_players)) {
+    add_option('able-player-count',$player_count);
+    return 'able-player-'.$this_player;
+  }
+  else {
+    // there's already at least one player
+    $this_player = $num_players + 1;
+    update_option('able-player-count',$this_player);
+    return 'able-player-'.$this_player;
+  }
+}
+
+function is_true($var) {
+
+  // $var is a Boolean parameter
+  // check for all variations that might be considered 'true'
+  // return true if... well, true
+  if ($var == '1' || $var == 'yes' || $var == 'true') {
+    return true;
+  }
+  else {
+    return false;
+  }
+}
+
 ?>
--- readme.txt
+++ readme.txt
@@ -24,21 +24,100 @@ This plug-in uses Able Player, an open-source fully-accessible cross-browser HTM
 
 == Instructions for Use ==
 
-This is an early pre-release version of this plugin, designed to make it possible and fairly easy to add an Able Player instance to a WordPress site.
+There are currently two ways to add an Able Player instance to a WordPress site:
 
-To use it, simply enter or paste any valid HTML5 Able Player code into your web page.
+1. Enter or paste any valid HTML5 Able Player code into your web page. Full documentation, including a list and explanation of all supported HTML5 attributes, is available on the [Able Player](http://ableplayer.github.io/ableplayer) project page on GitHub.
 
-Full documentation, including a list and explanation of all supported HTML5 attributes, is available on the [Able Player](http://ableplayer.github.io/ableplayer) project page on GitHub.
+2. Enter an [able-player] shortcode, in combination with one or more [able-source] shortcodes, plus one or more [able-track] shortcodes.
+
+= The [able-player] shortcode =
+
+The [able-player] shortcode is a container, so must be terminated by the [/able-player] shortcode. In between these opening and closing shortcodes, you must have at least one [able-source] shortcode.
+
+The [able-player] shortcode supports the following attributes:
+
+* type (required) - either "video" or "audio"
+* id - a unique id for the player (if omitted, one will be assigned)
+* autoplay - "true" or "false" (default is "false")
+* loop' - "true" or "false" (default is "false")
+* playsinline - "true" or "false" (default is "true")
+* hidecontrols - "true" or "false" (default is "false")
+* poster - the URL of a poster image, displayed before the user presses Play
+* width - a value in pixels (by default, the player will be sized to fit its container)
+* height - a value in pixels (by default, the height of the player will be in proportion to the width)
+* heading - The HTML heading level (1-6) of the visually hidden "Media Player" heading that precedes the player (for the benefit of screen reader users). If omitted, a heading level will be intelligently assigned based on context.
+* speed - "animals" or "arrows" (default is "animals")
+* start - start time at which to start playing the media, in seconds
+* volume - "0" to "10" (default is "7" to avoid overpowering screen reader audio)
+* seekinterval - number of seconds to forward/rewind.
+* nowplaying - "true" or "false" to include a "Selected Track" section within the media player (default is "false")
+
+= The [able-source] shortcode =
+
+The [able-source] shortcode is self-contained (i.e., it does not need a closing shortcode).
+
+The [able-source] shortcode supports the following attributes:
+
+* src - URL of the media file
+* type - the mime type of the media file (e.g., "audio/mpeg", "video/mp4")
+* desc-src - URL of the media file for the described version
+* sign-src - URL of the media file for synchronized sign language
+* youtube-id - 11-character YouTube ID
+* youtube-desc-id - 11-character YouTube ID of the described version
+* youtube-nocookie => "true" or "false" (use "true" to embed YouTube untracked, for added privacy)
+* vimeo-id - Vimeo ID
+* vimeo-desc-id - Vimeo ID for described version
+
+= The [able-track] shortcode =
+
+The [able-track] shortcode is self-contained (i.e., it does not need a closing shortcode).
+
+The [able-track] shortcode supports the following attributes:
+
+* kind - "captions", "subtitles", "chapters", "descriptions", or "metadata"
+* src - URL to the track file (must be a valid WebVTT file)
+* srclang - language code of the track language (e.g., "en" for English, "es" for Español)
+* label - A label to display in the captions/subtitles menu (e.g., "English")
+
+
+= Example 1 =
+
+This example uses HTML to add a video player to the page, with one source (an MP4 file) and four tracks (for captions, descriptions, and chapters in English; and subtitles in Spanish).
+
+<video id="able-player-1" data-able-player preload="auto" poster="path_to_image.jpg">
+  <source type="video/mp4" src="path_to_video.mp4">
+  <track kind="captions" src="path_to_captions.vtt" srclang="en" label="English">
+  <track kind="subtitles" src="path_to_subtitles.vtt" srclang="es" label="Español">
+  <track kind="descriptions" src="path_to_descriptions.vtt" srclang="en">
+  <track kind="chapters" src="path_to_chapters.vtt" srclang="en">
+</video>
+
+= Example 2 =
+
+This example is the same as Example 1, but uses shortcodes.
+
+[able-player type="video" id="able-player-1" poster="path_to_image.jpg">
+  [able-source type="video/mp4" src="path_to_video.mp4"]
+  [able-track kind="captions" src="path_to_captions.vtt" srclang="en" label="English"]
+  [able-track kind="subtitles" src="path_to_subtitles.vtt" srclang="es" label="Español"]
+  [able-track kind="descriptions" src="path_to_descriptions.vtt" srclang="en"]
+  [able-track kind="chapters" src="path_to_chapters.vtt" srclang="en"]
+[/able-player]
 
 
 == Next Steps ==
-1. Tighten CSS within Able Player so player styles are less likely to be overridden by WordPress theme styles.
-1. Provide a user interface by which authors can select and configure options through WordPress rather than typing in HTML.
+1. Reconsider shortcode implementation, as currently it offers no advantage whatsoever over HTML.
+1. Provide a user interface by which authors can select and configure default options through WordPress.
 1. Interface directly with existing WordPress media libraries so users can select their media files and other assets rather than typing in URLs.
-1. Consider whether and how best to support shortcodes.
+1. Test whether and how to pass third party shortcodes to select HTML attributes rather than URLs, and add relevant documentation to this README.
 
 == Changelog ==
 
 = 0.1 =
 * Initial version
+
+= 0.1.1 =
+* Add support for shortcodes
+
+
 
