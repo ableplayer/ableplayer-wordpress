e177b67a2be4777c4e5ab708ada0f6fa266b2186
Joe Dolson <design@joedolson.com>
2025-03-18 14:35:37 -0500

    Update versions for 1.2.1

--- init.php
+++ init.php
@@ -16,7 +16,7 @@
  * License:     MIT
  * License URI: https://github.com/ableplayer/ableplayer-wordpress/blob/master/LICENSE
  * Domain Path: lang
- * Version:     1.2.0
+ * Version:     1.2.1
  */
 
 if ( ! defined( 'ABSPATH' ) ) {
