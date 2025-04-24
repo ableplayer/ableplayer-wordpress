2df07c72f291fa1ba6d43fa5ce11a890e8a0a0f4
Joe Dolson <design@joedolson.com>
2025-04-24 09:44:41 -0500

    Also change path in init

--- init.php
+++ init.php
@@ -15,7 +15,7 @@
  * Text Domain: ableplayer
  * License:     MIT
  * License URI: https://github.com/ableplayer/ableplayer-wordpress/blob/master/LICENSE
- * Domain Path: lang
+ * Domain Path: /lang
  * Version:     1.2.1
  */
 
