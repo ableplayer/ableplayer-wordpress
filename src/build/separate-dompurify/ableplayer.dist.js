384d3ba600c4118ba5dcc9d0d59cf78fae978a67
Joe Dolson <design@joedolson.com>
2025-06-23 15:39:07 -0500

    Update build files to 4.6.0.

--- src/build/separate-dompurify/ableplayer.dist.js
+++ src/build/separate-dompurify/ableplayer.dist.js
@@ -3726,22 +3726,21 @@ var AblePlayerInstances = [];
 	};
 
 	AblePlayer.prototype.getDefaultWidth = function(which) {
-
+		let viewportMaxwidth = window.innerWidth;
 		// return default width of resizable elements
 		// these values are somewhat arbitrary, but seem to result in good usability
 		// if users disagree, they can resize (and resposition) them
 		if (which === 'transcript') {
-			return 450;
+			return ( viewportMaxwidth <= 450 ) ? viewportMaxwidth : 450;
 		}
 		else if (which === 'sign') {
-			return 400;
+			return ( viewportMaxwidth <= 400 ) ? viewportMaxwidth : 400;
 		}
 	};
 
 	AblePlayer.prototype.positionDraggableWindow = function (which, width) {
 
 		// which is either 'transcript' or 'sign'
-
 		var cookie, cookiePos, $window, windowPos;
 
 		cookie = this.getCookie();
@@ -11571,10 +11570,14 @@ if (typeof module !== "undefined" && module.exports) {
     $autoScrollLabel = $("<label>", {
       for: "autoscroll-transcript-checkbox-" + this.mediaId,
     }).text(this.tt.autoScroll);
-    this.$transcriptToolbar.append(
-      $autoScrollLabel,
-      this.$autoScrollTranscriptCheckbox
-    );
+	$autoScrollContainer = $( '<div>', {
+		'class': 'autoscroll-transcript'
+	});
+	$autoScrollContainer.append(
+		$autoScrollLabel,
+		this.$autoScrollTranscriptCheckbox
+	);
+    this.$transcriptToolbar.append( $autoScrollContainer );
 
     // Add field for selecting a transcript language
     // Only necessary if there is more than one language
@@ -13558,7 +13561,7 @@ if (typeof module !== "undefined" && module.exports) {
 
 	AblePlayer.prototype.addWindowMenu = function(which, $window, windowName) {
 
-		var thisObj, $windowAlert, menuId, $newButton, $buttonIcon, buttonImgSrc, $buttonImg,
+		var thisObj, $windowAlert, menuId, $newButton, $buttonIcon, buttonImgSrc,
 			$buttonLabel, tooltipId, $tooltip, $popup, menuId;
 
 		thisObj = this;
@@ -13600,15 +13603,16 @@ if (typeof module !== "undefined" && module.exports) {
 			});
 			$newButton.append($buttonIcon);
 		}
+		// Inexplicably, SVG images aren't working in the dragdrop UI.
 		else {
 			// use image
 			buttonImgSrc = this.rootPath + 'button-icons/' + this.toolbarIconColor + '/preferences.png';
-			$buttonImg = $('<img>',{
+			$buttonIcon = $('<img>',{
 				'src': buttonImgSrc,
 				'alt': '',
 				'role': 'presentation'
 			});
-			$newButton.append($buttonImg);
+			$newButton.append($buttonIcon);
 		}
 
 		// add the visibly-hidden label for screen readers that don't support aria-label on the button
@@ -13625,18 +13629,17 @@ if (typeof module !== "undefined" && module.exports) {
 		}).hide();
 		$newButton.on('mouseenter focus',function(e) {
 			var label = $(this).attr('aria-label');
-			// get position of this button
-			var position = $(this).position();
-			var buttonHeight = $(this).height();
-			var buttonWidth = $(this).width();
-			var tooltipY = position.top - buttonHeight - 5;
+			var tooltip = AblePlayer.localGetElementById($newButton[0], tooltipId).text(label);
+			// get height of the tooltip
+			var tooltipHeight = tooltip.height();
+			var tooltipY = ( tooltipHeight + 2 ) * -1;
 			var tooltipX = 0;
 			var tooltipStyle = {
-				left: '',
-				right: tooltipX + 'px',
+				right: '',
+				left: tooltipX + 'px',
 				top: tooltipY + 'px'
 			};
-			var tooltip = AblePlayer.localGetElementById($newButton[0], tooltipId).text(label).css(tooltipStyle);
+			tooltip.css(tooltipStyle);
 			thisObj.showTooltip(tooltip);
 			$(this).on('mouseleave blur',function() {
 				AblePlayer.localGetElementById($newButton[0], tooltipId).text('').hide();
@@ -13856,7 +13859,7 @@ if (typeof module !== "undefined" && module.exports) {
 		else {
 			// first, be sure window is on top
 			this.updateZIndex(which);
-			popupTop = $windowButton.position().top + $windowButton.outerHeight();
+			popupTop = $toolbar.outerHeight() - 1;
 			$windowPopup.css('top', popupTop);
 			$windowPopup.show(200,'',function() {
 				$windowButton.attr('aria-expanded','true');
@@ -13941,7 +13944,7 @@ if (typeof module !== "undefined" && module.exports) {
 				this.dragDevice = 'mouse';
 			}
 			this.startDrag(which, $window);
-			$windowPopup.hide().parent().trigger('focus');
+			$windowPopup.hide().parent().attr( 'tabindex', '-1' ).trigger('focus');
 		}
 		else if (choice == 'resize') {
 			// resize through the menu uses a form, not drag
@@ -13982,16 +13985,6 @@ if (typeof module !== "undefined" && module.exports) {
 		}
 		else if (which === 'sign') {
 			$windowPopup = this.$signPopup;
-		}
-
-		if (!this.showedAlert(which)) {
-			this.showAlert(this.tt.windowMoveAlert,which);
-			if (which === 'transcript') {
-				this.showedTranscriptAlert = true;
-			}
-			else if (which === 'sign') {
-				this.showedSignAlert = true;
-			}
 		}
 
 		// if window's popup menu is open, close it
