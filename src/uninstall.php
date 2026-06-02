d802b9f90c50859578245933a148f0ca22b2d57a
Joe Dolson <design@joedolson.com>
2026-06-02 02:15:24 -0500

    Update uninstall.php

--- src/uninstall.php
+++ src/uninstall.php
@@ -12,7 +12,7 @@
 if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 	exit();
 } else {
-    delete_option( 'able_player_count' );
-    delete_option( 'able_show_playground_intro' );
-    delete_option( 'ableplayer_settings' );
+	delete_option( 'able_player_count' );
+	delete_option( 'able_show_playground_intro' );
+	delete_option( 'ableplayer_settings' );
 }
